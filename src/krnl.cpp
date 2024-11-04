71aea13bd0a9543f3d46345903a32b5cbb2ced4a
pyuvaraj37 <ryuvaraj37@gmail.com>
2024-11-04 12:34:50 -0800

    key-value store

--- src/krnl.cpp
+++ src/krnl.cpp
@@ -1,31 +1,257 @@
 #include "krnl.hpp"
 
-// void krnl(int* a, int* b, int* c, int size) {
-//     for (int i = 0; i < size; i++){ 
-//         #pragma HLS PIPELINE OFF
-//         c[i] = a[i] + b[i];
-//     }
-// }
+const int NUM_NODES = 2; 
 
-// void krnl_top(int* a, int* b, int* c, int size) {
+void rdma_read(
+    int s_axi_lqpn,
+    ap_uint<64> s_axi_laddr,
+    ap_uint<64> s_axi_raddr,
+    int s_axi_len,
+    hls::stream<pkt256>& m_axis_tx_meta
+){
+    //#pragma HLS dataflow
+    #pragma HLS inline off
+    #pragma HLS pipeline II=1
+    pkt256 tx_meta;
 
-//     #pragma HLS INTERFACE m_axi port=a bundle=gmem1
-//     #pragma HLS INTERFACE m_axi port=b bundle=gmem1
-//     #pragma HLS INTERFACE m_axi port=c bundle=gmem2
-//     #pragma HLS INTERFACE s_axilite port=size
+    /*RDMA OP*/
+    tx_meta.data.range(2,0) = 0x00000000; 
+    /*lQPN*/
+    tx_meta.data.range(26,3) = s_axi_lqpn; 
+    /*
+    lAddr
+    */
+    tx_meta.data.range(74, 27) = s_axi_laddr; 
+    /*rAddr*/
+    tx_meta.data.range(122, 75) = s_axi_raddr; 
+    //+(itt*4)
+    /*len*/
+    tx_meta.data.range(154, 123) = s_axi_len;
+    m_axis_tx_meta.write(tx_meta);
+    
+}
 
-//     krnl(a, b, c, size);
-// }
+void rdma_write(
+    int s_axi_lqpn,
+    ap_uint<64> s_axi_laddr,
+    ap_uint<64> s_axi_raddr,
+    int s_axi_len,
+    ap_uint<64>  write_value,
+    hls::stream<pkt256>& m_axis_tx_meta, 
+    hls::stream<pkt64>& m_axis_tx_data
+){
+    //#pragma HLS dataflow
+    #pragma HLS inline off
+    #pragma HLS pipeline II=1
+    
+    pkt256 tx_meta;
+    pkt64 tx_data;
+
+    /*RDMA OP*/
+    tx_meta.data.range(2,0) = 0x00000001; 
+    /*lQPN*/
+    tx_meta.data.range(26,3) = s_axi_lqpn; 
+    /*
+    lAddr
+    if 0 writes from tx_data. 
+    */
+    tx_meta.data.range(74, 27) = s_axi_laddr; 
+    /*rAddr*/
+    tx_meta.data.range(122, 75) = s_axi_raddr; 
+    //+(itt*4)
+    /*len*/
+    tx_meta.data.range(154, 123) = s_axi_len;
+    
+    m_axis_tx_meta.write(tx_meta);
+
+    //Write data only if laddr is 0
+    if (s_axi_laddr == 0) {
+        tx_data.data(63, 0) = write_value;
+        tx_data.keep(7, 0) = 0xff;
+        tx_data.last = 1; 
+        m_axis_tx_data.write(tx_data);
+    }
+
+    
+}
 
 
-void krnl(data_t* a, data_t* b, data_t* c) {
-    #pragma HLS INTERFACE m_axi port = a bundle = gmem0
-    #pragma HLS INTERFACE m_axi port = b bundle = gmem1
-    #pragma HLS INTERFACE m_axi port = c bundle = gmem2
+void rdma_write_through(
+    int s_axi_lqpn,
+    ap_uint<64> s_axi_laddr,
+    ap_uint<64> s_axi_raddr,
+    int s_axi_len,
+    ap_uint<64>  write_value,
+    hls::stream<pkt256>& m_axis_tx_meta, 
+    hls::stream<pkt64>& m_axis_tx_data
+){
+    //#pragma HLS dataflow
+    #pragma HLS inline off
+    #pragma HLS pipeline II=1
+    #pragma HLS INTERFACE axis port = m_axis_tx_meta
+    #pragma HLS INTERFACE axis port = m_axis_tx_data
+    
+    pkt256 tx_meta;
+    pkt64 tx_data;
 
-    for (int i = 0; i < DATA_SIZE; i++) {
-        #pragma HLS UNROLL //factor=2
-        c[i] = a[i] + b[i];
+    /*RDMA OP*/
+    tx_meta.data.range(2,0) = 0x00000004; 
+    /*lQPN*/
+    tx_meta.data.range(26,3) = s_axi_lqpn; 
+    /*
+    lAddr
+    if 0 writes from tx_data. 
+    */
+    tx_meta.data.range(74, 27) = s_axi_laddr; 
+    /*rAddr*/
+    tx_meta.data.range(122, 75) = s_axi_raddr; 
+    //+(itt*4)
+    /*len*/
+    tx_meta.data.range(154, 123) = s_axi_len;
+    
+
+    m_axis_tx_meta.write(tx_meta);
+
+    //Write data only if laddr is 0
+    if (s_axi_laddr == 0) {
+        tx_data.data.range(63, 0) = write_value;
+        tx_data.keep(7, 0) = 0xff;
+        tx_data.last = 1; 
+        
+        m_axis_tx_data.write(tx_data);
     }
 
 }
+
+void rdma_bram_read(
+    int s_axi_lqpn,
+    ap_uint<64> s_axi_laddr,
+    ap_uint<64> s_axi_raddr,
+    int s_axi_len,
+    hls::stream<pkt256>& m_axis_tx_meta
+){
+    //#pragma HLS dataflow
+    #pragma HLS inline off
+    #pragma HLS pipeline II=1
+    pkt256 tx_meta;
+
+    /*RDMA OP*/
+    tx_meta.data.range(2,0) = 0x00000002; 
+    /*lQPN*/
+    tx_meta.data.range(26,3) = s_axi_lqpn; 
+    /*
+    lAddr
+    */
+    tx_meta.data.range(74, 27) = s_axi_laddr; 
+    /*rAddr*/
+    tx_meta.data.range(122, 75) = s_axi_raddr; 
+    //+(itt*4)
+    /*len*/
+    tx_meta.data.range(154, 123) = s_axi_len;
+    m_axis_tx_meta.write(tx_meta);
+    
+}
+
+void rdma_bram_write(
+    int s_axi_lqpn,
+    ap_uint<64> s_axi_laddr,
+    ap_uint<64> s_axi_raddr,
+    int s_axi_len,
+    ap_uint<64>  write_value,
+    hls::stream<pkt256>& m_axis_tx_meta, 
+    hls::stream<pkt64>& m_axis_tx_data
+){
+    //#pragma HLS dataflow
+    #pragma HLS inline off
+    #pragma HLS pipeline II=1
+    #pragma HLS INTERFACE axis port = m_axis_tx_meta
+    #pragma HLS INTERFACE axis port = m_axis_tx_data
+    
+    pkt256 tx_meta;
+    pkt64 tx_data;
+
+    /*RDMA OP*/
+    tx_meta.data.range(2,0) = 0x00000003; 
+    /*lQPN*/
+    tx_meta.data.range(26,3) = s_axi_lqpn; 
+    /*
+    lAddr
+    if 0 writes from tx_data. 
+    */
+    tx_meta.data.range(74, 27) = s_axi_laddr; 
+    /*rAddr*/
+    tx_meta.data.range(122, 75) = s_axi_raddr; 
+    //+(itt*4)
+    /*len*/
+    tx_meta.data.range(154, 123) = s_axi_len;
+    
+
+    m_axis_tx_meta.write(tx_meta);
+
+    //Write data only if laddr is 0
+    if (s_axi_laddr == 0) {
+        tx_data.data.range(63, 0) = write_value;
+        tx_data.keep(7, 0) = 0xff;
+        tx_data.last = 1; 
+        
+        m_axis_tx_data.write(tx_data);
+    }
+
+}
+
+
+void test_krnl(
+    //Outgoing RDMA
+    hls::stream<pkt256>& m_axis_tx_meta, 
+    hls::stream<pkt64>& m_axis_tx_data,
+    hls::stream<pkt64>& s_axis_tx_status,
+
+    //Local BRAM 
+    hls::stream<pkt256>& m_axis_bram_write_cmd, 
+    hls::stream<pkt256>& m_axis_bram_read_cmd,
+    hls::stream<pkt512>& m_axis_bram_write_data,
+    hls::stream<pkt512>& s_axis_bram_read_data,
+
+    //Incoming 
+    hls::stream<pkt64>& s_axis_update,
+
+    //Local HBM
+    int *network_ptr
+
+    //Hash-Table
+    hls::stream<htLookupReq>& lup_request,
+    //look-up resp
+    //update req
+    //update resp
+
+
+    //Book keeping
+    int myBoardNum
+) {
+
+    #pragma HLS INTERFACE axis port = m_axis_tx_meta
+    #pragma HLS INTERFACE axis port = m_axis_tx_data
+    #pragma HLS INTERFACE axis port = s_axis_tx_status
+
+    #pragma HLS INTERFACE axis port = m_axis_bram_write_cmd
+    #pragma HLS INTERFACE axis port = m_axis_bram_read_cmd
+    #pragma HLS INTERFACE axis port = m_axis_bram_write_data
+    #pragma HLS INTERFACE axis port = s_axis_bram_read_data
+
+    #pragma HLS INTERFACE axis port = s_axis_update
+
+
+    pkt64 status; 
+    if (!s_axis_tx_status.empty()) {
+        s_axis_tx_status.read(status);
+    }
+
+    #pragma HLS DATAFLOW
+
+
+
+    
+    return; 
+}
+
+
--- src/test_krnl.cpp
+++ src/test_krnl.cpp
@@ -1,16 +1,68 @@
 #include "krnl.hpp"
 #include <iostream>
 
+
+
+// void test_krnl(
+//     hls::stream<pkt256>& m_axis_tx_meta, 
+//     hls::stream<pkt64>& m_axis_tx_data,
+//     hls::stream<pkt64>& s_axis_tx_status,
+
+//     hls::stream<pkt256>& m_axis_bram_write_cmd, 
+//     hls::stream<pkt256>& m_axis_bram_read_cmd,
+//     hls::stream<pkt512>& m_axis_bram_write_data,
+//     hls::stream<pkt512>& s_axis_bram_read_data,
+
+//     hls::stream<pkt64>& s_axis_update,
+//     int myBoardNum,
+//     int RDMA_TYPE, 
+//     int exec, 
+//     int *network_ptr
+// );
+
 int main() {
 
-    int n = 2; 
-    int a[n] = {1, 1}; 
-    int b[n] = {1, 1};
-    int c[n];
+    int myBoardNum = 1, RDMA_TYPE = 4, exec = 1000000;
+    int network_ptr[100];
 
-    krnl(a, b, c);
+    hls::stream<pkt256> s_axis_tx_meta; 
+    hls::stream<pkt64> s_axis_tx_data;
+    hls::stream<pkt64> m_axis_tx_status;
 
-    std::cout << "C  = " << c[0] << std::endl; 
-    std::cout << "C  = " << c[1] << std::endl; 
+    hls::stream<pkt256> s_axis_bram_write_cmd; 
+    hls::stream<pkt256> s_axis_bram_read_cmd;
+    hls::stream<pkt512> s_axis_bram_write_data;
+    hls::stream<pkt512> m_axis_bram_read_data;
+
+    hls::stream<pkt64> m_axis_update;
+
+    //Test HBM
+    network_ptr[0] = exec;
+
+
+    //Test BRAM
+    pkt512 bram_data;
+    bram_data.data = exec;
+    m_axis_bram_read_data.write(bram_data);
+
+    //Test Write-Through
+    pkt64 wt_data;
+    wt_data.data = exec; 
+    m_axis_update.write(wt_data);
+
+    test_krnl(
+        s_axis_tx_meta,
+        s_axis_tx_data,
+        m_axis_tx_status,
+        s_axis_bram_write_cmd,
+        s_axis_bram_read_cmd,
+        s_axis_bram_write_data,
+        m_axis_bram_read_data,
+        m_axis_update,
+        myBoardNum, 
+        RDMA_TYPE,
+        exec,
+        network_ptr
+    );
 
 }
