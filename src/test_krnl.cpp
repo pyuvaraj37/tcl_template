f50c40dd2cef9b973e1439b3fbe20416295fe6d2
Clarity Shimoniak <clarity.shimoniak@email.ucr.edu>
2024-11-07 16:49:56 -0800

    Removed unused code

--- src/test_krnl.cpp
+++ src/test_krnl.cpp
@@ -3,26 +3,7 @@
 #include "hls_stream.h"
 
 
-
-// void test_krnl(
-//     hls::stream<pkt256>& m_axis_tx_meta, 
-//     hls::stream<pkt64>& m_axis_tx_data,
-//     hls::stream<pkt64>& s_axis_tx_status,
-
-//     hls::stream<pkt256>& m_axis_bram_write_cmd, 
-//     hls::stream<pkt256>& m_axis_bram_read_cmd,
-//     hls::stream<pkt512>& m_axis_bram_write_data,
-//     hls::stream<pkt512>& s_axis_bram_read_data,
-
-//     hls::stream<pkt64>& s_axis_update,
-//     int myBoardNum,
-//     int RDMA_TYPE, 
-//     int exec, 
-//     int *network_ptr
-// );
-
 int main() {
-
     int myBoardNum = 1, RDMA_TYPE = 4, exec = 1000000;
     int network_ptr[100];
 
@@ -37,10 +18,9 @@ int main() {
 
     hls::stream<pkt64> m_axis_update;
 
-    //Set up inital state
 
-    //Test HBM
-    network_ptr[0] = exec;
+    //TODO: Set up inital state
+
 
     //Run kernel
     test_krnl(
@@ -58,5 +38,7 @@ int main() {
         network_ptr
     );
 
+
     //TODO: Check results
+
 }
